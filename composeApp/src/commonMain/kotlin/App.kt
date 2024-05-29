b2c26823c450b3398c38f948b7ce0ccaf428df03
정승규 <jsk41755@naver.com>
2024-05-29 13:48:25 +0900

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -0,0 +1,29 @@
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+val lightRedColor = Color(color = 0xFFF57D88)
+val darkRedColor = Color(color = 0xFF77000B)
+
+@Composable
+@Preview
+fun App() {
+    val lightColors = lightColorScheme(
+        primary = lightRedColor,
+        onPrimary = darkRedColor,
+        primaryContainer = lightRedColor,
+        onPrimaryContainer = darkRedColor
+    )
+    val darkColors = darkColorScheme(
+        primary = lightRedColor,
+        onPrimary = darkRedColor,
+        primaryContainer = lightRedColor,
+        onPrimaryContainer = darkRedColor
+    )
+    val colors by mutableStateOf(
+        if (isSystemInDarkTheme()) darkColors else lightColors
+    )
+}
