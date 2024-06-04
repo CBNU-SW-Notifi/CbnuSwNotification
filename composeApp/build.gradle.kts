86d16ff9ff3a1f5116943e357cafabd07d8894c1
정승규 <jsk41755@naver.com>
2024-06-04 21:03:00 +0900

    fix:: iOS Room Directory changed (HomeDirectory -> DocumentDirectory)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -41,7 +41,7 @@ kotlin {
         iosTarget.binaries.framework {
             baseName = "ComposeApp"
             isStatic = true
-            export(libs.kmpNotifier)
+            //export(libs.kmpNotifier)
         }
     }
 
--- composeApp/src/iosMain/kotlin/database/getPeopleDatabase.kt
+++ composeApp/src/iosMain/kotlin/database/getPeopleDatabase.kt
@@ -2,11 +2,26 @@ package database
 
 import androidx.room.Room
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
-import platform.Foundation.NSHomeDirectory
-import database.instantiateImpl
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSLog
+import platform.Foundation.NSURL
+import platform.Foundation.NSUserDomainMask
 
 fun getPeopleDatabase(): PeopleDatabase {
-    val dbFile = NSHomeDirectory() + "/people.db"
+    val fileManager = NSFileManager.defaultManager()
+    val urls = fileManager.URLsForDirectory(NSDocumentDirectory, NSUserDomainMask)
+    val documentsDirectory = urls.first() as NSURL
+    val dbFile = documentsDirectory.path + "/people.db"
+    NSLog("Database file path: $dbFile")
+
+    if (!fileManager.fileExistsAtPath(dbFile)) {
+        NSLog("Database file does not exist. Creating new file.")
+        fileManager.createFileAtPath(dbFile, null, null)
+    } else {
+        NSLog("Database file already exists.")
+    }
+
     return Room.databaseBuilder<PeopleDatabase>(
         name = dbFile,
         factory = { PeopleDatabase::class.instantiateImpl() }
