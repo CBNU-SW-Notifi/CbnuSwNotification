81a2693370e1d85b7724feed07b58d0651330cf7
정승규 <jsk41755@naver.com>
2024-06-14 21:25:39 +0900

    feature : 이미지 로딩 구현

--- composeApp/src/commonMain/kotlin/presentation/screen/PostDetailScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/screen/PostDetailScreen.kt
@@ -3,19 +3,15 @@ package presentation.screen
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.Divider
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -23,16 +19,24 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil3.compose.AsyncImage
+import coil3.compose.AsyncImagePainter
 import coil3.compose.LocalPlatformContext
+import coil3.compose.rememberAsyncImagePainter
 import getOpenUrl
 import org.koin.compose.getKoin
+import presentation.components.LoadingAnimation
 import presentation.screen.home.PostDetailViewModel
 
 @Composable
@@ -50,7 +54,7 @@ fun PostDetailScreen(postId: Int, navController: NavController) {
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp),
+                .padding(8.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             item {
@@ -58,27 +62,58 @@ fun PostDetailScreen(postId: Int, navController: NavController) {
                     text = "Title: ${detail.title}",
                     style = MaterialTheme.typography.headlineMedium
                 )
+
+                HorizontalDivider(color = Color.Gray, thickness = 1.dp)
             }
 
             detail.imageUrls?.let { urls ->
                 items(urls) { imageUrl ->
                     if (imageUrl.isNotBlank()) {
-                        coil3.compose.AsyncImage(
-                            modifier = Modifier
-                                .fillMaxWidth(),
+                        var alpha by remember { mutableStateOf(1f) }
+                        var showLoadingAnimation by remember { mutableStateOf(true) }
+
+                        AsyncImage(
                             model = imageUrl,
                             contentDescription = null,
                             contentScale = ContentScale.Crop,
+                            modifier = Modifier.fillMaxWidth(),
+                            onState = { state ->
+                                when (state) {
+                                    is AsyncImagePainter.State.Loading -> {
+                                        showLoadingAnimation = true
+                                        alpha = 0.5f
+                                    }
+                                    is AsyncImagePainter.State.Success -> {
+                                        showLoadingAnimation = false
+                                        alpha = 1f
+                                    }
+                                    is AsyncImagePainter.State.Error -> {
+                                        showLoadingAnimation = false
+                                        alpha = 1f
+                                    }
+                                    else -> {}
+                                }
+                            }
+                        )
 
-                            onError = {
-                                println("load error")
-                            },
-                            onLoading = {
-                                println("load onLoading")
-                            },
-                            onSuccess = {
-                                println("load onSuccess")
-                            },
+                        if (showLoadingAnimation) {
+                            Box(
+                                contentAlignment = Alignment.Center,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .heightIn(min = 200.dp)
+                            ) {
+                                LoadingAnimation()
+                            }
+                        }
+
+                        Image(
+                            painter = rememberAsyncImagePainter(imageUrl),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .alpha(alpha),
+                            contentScale = ContentScale.Crop
                         )
                     }
                 }
