a8348dda6df3e25c30702da7d9388f599d85461c
정승규 <jsk41755@naver.com>
2024-06-14 22:38:09 +0900

    trash code 제거

--- composeApp/src/commonMain/kotlin/presentation/screen/PostDetailScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/screen/PostDetailScreen.kt
@@ -1,6 +1,5 @@
 package presentation.screen
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Arrangement
@@ -27,7 +26,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
@@ -40,7 +38,6 @@ import androidx.navigation.NavController
 import coil3.compose.AsyncImage
 import coil3.compose.AsyncImagePainter
 import coil3.compose.LocalPlatformContext
-import coil3.compose.rememberAsyncImagePainter
 import getOpenUrl
 import org.koin.compose.getKoin
 import presentation.components.LoadingAnimation
@@ -148,15 +145,6 @@ fun PostDetailScreen(postId: Int, navController: NavController) {
                                     LoadingAnimation()
                                 }
                             }
-
-                            Image(
-                                painter = rememberAsyncImagePainter(imageUrl),
-                                contentDescription = null,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .alpha(alpha),
-                                contentScale = ContentScale.Crop
-                            )
                         }
                     }
                 }
